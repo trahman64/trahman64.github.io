61f68a9fbed5143a95e1344040f137552a62f31a
trahman64 <156981381+trahman64@users.noreply.github.com>
2025-09-04 04:51:22 +0000

    Deploying to gh-pages from @ trahman64/trahman64.github.io@b15c664270ee60082cf9844acbabfc4038f1eea6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,8 +10,8 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-projects",
-          title: "Projects",
-          description: "Check out some of my work!.",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
