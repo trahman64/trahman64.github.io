61f98bd673584bfca246e32e8e51cff5ef9a552c
trahman64 <156981381+trahman64@users.noreply.github.com>
2025-09-04 04:24:17 +0000

    Deploying to gh-pages from @ trahman64/trahman64.github.io@bada81733e74ff4158df8216ffb8b88248a112df 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -469,6 +469,13 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/tasmia-rahman-b4269321a", "_blank");
         },
       },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.alberteinstein.com/", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
