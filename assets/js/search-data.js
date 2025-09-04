fe82a7d51e76d9814db8021236cbec7915288bc0
trahman64 <156981381+trahman64@users.noreply.github.com>
2025-09-04 04:09:57 +0000

    Deploying to gh-pages from @ trahman64/trahman64.github.io@5696947b2fba4a1545749ec7c70fb13fbcec5a56 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,18 +10,11 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+          title: "Projects",
+          description: "Check out some of my work!.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
           },
         },{id: "nav-cv",
           title: "cv",
